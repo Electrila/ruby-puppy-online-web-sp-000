9f101d3fa44de6b6a1755085e84c0fe675072549
Megann Comstock <megann.elizabeth.king@gmail.com>
2021-05-23 00:06:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,24 @@
-# Add your code here
+class Dog
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    def save(self.name)
+      @@all << self.name
+    end
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.clear_all
+    @@all.clear
+  end
+  
+  def self.print_all
+    @@all.each { |dog| puts dog }
+  end
+  
+end
